c32e45bb233acb79fca9625bba5adeb1080fefdd
kmktu <kmktu@naver.com>
2020-05-07 16:30:19 +0900

    back up

--- MLP_testing.py
+++ MLP_testing.py
@@ -1,38 +1,38 @@
-# from sklearn.datasets import make_classification
-# from sklearn.multioutput import MultiOutputClassifier
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.utils import shuffle
-# import numpy as np
+from sklearn.datasets import make_classification
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.utils import shuffle
+import numpy as np
+
+X, y1 = make_classification(n_samples=10, n_features=100, n_informative=30,
+                            n_classes=3, random_state=1)
+y2 = shuffle(y1, random_state=1)
+y3 = shuffle(y1, random_state=2)
+Y = np.vstack((y1, y2, y3)).T
+n_samples, n_features = X.shape
+n_outputs = Y.shape[1]
+n_classes = 3
+forest = RandomForestClassifier(random_state=1)
+multi_target_forest = MultiOutputClassifier(forest, n_jobs=-1)
+multi_target_forest.fit(X, Y).predict(X)
+
+print(multi_target_forest.predict_proba(X))
+
+
+# import tensorflow as tf
 #
-# X, y1 = make_classification(n_samples=10, n_features=100, n_informative=30,
-#                             n_classes=3, random_state=1)
-# y2 = shuffle(y1, random_state=1)
-# y3 = shuffle(y1, random_state=2)
-# Y = np.vstack((y1, y2, y3)).T
-# n_samples, n_features = X.shape
-# n_outputs = Y.shape[1]
-# n_classes = 3
-# forest = RandomForestClassifier(random_state=1)
-# multi_target_forest = MultiOutputClassifier(forest, n_jobs=-1)
-# multi_target_forest.fit(X, Y).predict(X)
+# # Creates a graph.
+# c = []
+# for d in ['/device:GPU:0', '/device:GPU:1']:
+#   with tf.device(d):
+#     a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3])
+#     b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2])
+#     c.append(tf.matmul(a, b))
+# with tf.device('/cpu:0'):
+#   sum = tf.add_n(c)
 #
-# print(multi_target_forest.predict_proba(X))
-
-
-import tensorflow as tf
-
-# Creates a graph.
-c = []
-for d in ['/device:GPU:0', '/device:GPU:1']:
-  with tf.device(d):
-    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3])
-    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2])
-    c.append(tf.matmul(a, b))
-with tf.device('/cpu:0'):
-  sum = tf.add_n(c)
-
-# Creates a session with log_device_placement set to True.
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
-
-# Runs the op.
-print(sess.run(sum))
+# # Creates a session with log_device_placement set to True.
+# sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+#
+# # Runs the op.
+# print(sess.run(sum))
